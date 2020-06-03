3c0a98906cb780e06e09f2ac0e654bcb34894cbe
omarhajjouji <omarhajjouji@gmail.com>
2020-06-03 16:58:19 +0100

    done

--- articles-indexer.py
+++ articles-indexer.py
@@ -3,9 +3,13 @@ from bs4 import BeautifulSoup
 import random
 import sqlite3
 import traceback
-import urllib.parse
 import pandas
 
+import urllib
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import time
+from selenium.webdriver.common.by import By
 
 desktop_agents = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
                  'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
@@ -18,13 +22,41 @@ desktop_agents = ['Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML
                  'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.99 Safari/537.36',
                  'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:50.0) Gecko/20100101 Firefox/50.0']
 
-web-site="https://towardsdatascience.com/"
+site="https://towardsdatascience.com/tagged/"
+labels=["data-science","ai","deep-learning","machine-learning","artificial-intelligence"]
+path_to_chromedriver = r"C:\Users\amrou\Google Drive\python work\chromedriver.exe"
+options = webdriver.ChromeOptions()
+browser = webdriver.Chrome(executable_path = path_to_chromedriver,chrome_options=options)
+path=r"articles.csv"
+
+
 
 def random_headers():
 	return {'User-Agent': random.choice(desktop_agents),'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8'}
 
 
-def indexPage(link):
+def indexPage(url):
 	#see block note for algorithme
+        browser.get(url)
+        etr=0
+        while etr<500:
+                browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(1)
+                etr+=1
+        time.sleep(2)
+        content=browser.find_elements_by_css_selector('a[class=""]')
+        links=map(lambda x:x.get_attribute("href"),content)
+        r=open(path,"r+")
+        s=0
+        for i in links:
+                if(i and not(i in r)):
+                        r.write(str(i)+"\n")
+                        s+=1
+        r.close()
+        return s
 
- 		
+count=0
+for label in labels:
+        count+=indexPage(site+label)
+browser.quit()
+print(count)
